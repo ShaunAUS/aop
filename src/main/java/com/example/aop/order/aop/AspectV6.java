8455a02e4f3166b1fb1a9ec5c0bda26cb3ed0d50
ShaunAUS <wnsqud70@gmail.com>
2024-06-17 13:56:45 +0900

    어드바이스 종류

--- src/main/java/com/example/aop/order/aop/AspectV6.java
+++ src/main/java/com/example/aop/order/aop/AspectV6.java
@@ -8,7 +8,6 @@ import org.aspectj.lang.annotation.*;
 @Slf4j
 @Aspect
 public class AspectV6 {
-/*
     @Around("com.example.aop.order.aop.Pointcuts.allOrder()") // AOP 대상 설정
     public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable {
         log.info("[log] {}", joinPoint.getSignature()); // join point 시그니처
@@ -16,18 +15,25 @@ public class AspectV6 {
     }
 
     //example.aop.order 패키지 아래에 있으면서 + 클래스 이름 패턴이 *Service 포인트컷 설정
+/*
     @Around("com.example.aop.order.aop.Pointcuts.orderServiceAndService()")
     public Object doTransaction(ProceedingJoinPoint joinPoint) throws Throwable {
 
         try {
+            //@Before
             log.info("[트랜잭션 시작] {}", joinPoint.getSignature());
             Object result = joinPoint.proceed();
+
+            //@AfterReturning
             log.info("[트랜잭션 시작] {}", joinPoint.getSignature());
             return result;
         } catch (Exception e) {
+            //@AfterThrowing
             log.info("[트랜잭션 콜백] {}", joinPoint.getSignature());
             throw e;
         } finally {
+
+            //@After
             log.info("[리소스 릴리즈] {}", joinPoint.getSignature());
         }
     }
