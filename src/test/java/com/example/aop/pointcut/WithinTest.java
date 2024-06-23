e74e8d44027f6bae5ec258d311a34dac4b30c847
ShaunAUS <wnsqud70@gmail.com>
2024-06-23 23:56:58 +0900

    @within

---
+++ src/test/java/com/example/aop/pointcut/WithinTest.java
@@ -0,0 +1,55 @@
+package com.example.aop.pointcut;
+
+import com.example.member.MemberServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.lang.reflect.Method;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@SpringBootTest
+public class WithinTest {
+
+    AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+    Method helloMethod;
+
+    @BeforeEach
+    public void init() throws NoSuchMethodException {
+        helloMethod = MemberServiceImpl.class.getMethod("hello", String.class);
+    }
+
+    @Test
+    void withinExact() {
+        pointcut.setExpression("within(com.example.member.MemberServiceImpl)");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+    @Test
+    void withinStar() {
+        pointcut.setExpression("within(com.example.member.*Service*)");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    void withinSubPackage() {
+        pointcut.setExpression("within(com.example..*)");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    @DisplayName("타켓의 타입에만 직접 적용, 인터페이스를 선정하면 안된다.") void withinSuperTypeFalse() {
+        pointcut.setExpression("within(com.example.member.MemberService)");
+        assertThat(pointcut.matches(helloMethod,
+                MemberServiceImpl.class)).isFalse();
+    }
+    @Test
+    @DisplayName("execution은 타입 기반, 인터페이스를 선정 가능.") void executionSuperTypeTrue() {
+        pointcut.setExpression("execution(* com.example.member.MemberService.*(..))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+}
