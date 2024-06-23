a55096c90d780fa300c7e46ce89efec325543c63
ShaunAUS <wnsqud70@gmail.com>
2024-06-23 23:55:15 +0900

    @Annotation

---
+++ src/test/java/com/example/aop/pointcut/AnnotationTest.java
@@ -0,0 +1,36 @@
+package com.example.aop.pointcut;
+
+import com.example.member.MemberService;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Slf4j
+@Import(AnnotationTest.AnnotationAspect.class)
+@SpringBootTest
+public class AnnotationTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Test
+    void success() {
+        log.info("memberService Proxy={}", memberService.getClass());
+        memberService.hello("helloA");
+    }
+
+    @Slf4j
+    @Aspect
+    static class AnnotationAspect {
+        @Around("@annotation(com.example.member.annotation.MethodAop)")
+        public Object doAtAnnotation(ProceedingJoinPoint joinPoint) throws Throwable {
+            log.info("[@annotation] {}", joinPoint.getSignature());
+            return joinPoint.proceed();
+        }
+    }
+}
