9f31036595f97def867b237b5dfce6578090b194
ShaunAUS <wnsqud70@gmail.com>
2024-06-23 23:55:28 +0900

    기본 설정

---
+++ src/main/java/com/example/member/MemberService.java
@@ -0,0 +1,5 @@
+package com.example.member;
+
+public interface MemberService {
+    String hello(String param);
+}
---
+++ src/main/java/com/example/member/annotation/ClassAop.java
@@ -0,0 +1,13 @@
+package com.example.member.annotation;
+
+import javax.swing.text.Element;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ClassAop {
+
+}
